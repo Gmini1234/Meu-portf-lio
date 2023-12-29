f164fab346000b318703c708ab433d552e5dd0ba
Gabriel Lobo <145868621+Gmini1234@users.noreply.github.com>
2023-12-29 14:40:01 -0300

    Arquivos portfolio

---
+++ reveal.js
@@ -0,0 +1,25 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('#sobre-mim',
+{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('#experiencias',
+{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('#projetos',
+{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('#rodape',
+{
+  duration: 2000,
+  distance: '90px'
+})
